8a58a0628d968c80228b7fb6f251e8e3ebb35bc3
Christopher Su <christophersu9@gmail.com>
2014-08-06 19:56:04 -0700

    remove debug

--- background.js
+++ background.js
@@ -2,7 +2,6 @@ chrome.commands.onCommand.addListener(function(command) {
     if (command == "bookmark-tab") {
         chrome.tabs.getSelected(null, function(tab){
             chrome.storage.sync.get(["pinboardUsername", "pinboardPassword"], function(items) {
-                console.log('hi');
                 var xhr = new XMLHttpRequest();
                 xhr.open("GET", "https://api.pinboard.in/v1/posts/add?url=" + tab.url + "&description=" + tab.title, true);
                 xhr.setRequestHeader("Authorization", "Basic " + btoa(items.pinboardUsername + ":" + items.pinboardPassword));
