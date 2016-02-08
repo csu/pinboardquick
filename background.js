979d3248929e64ae357d074223c7c503ac336d99
Christopher Su <christophersu9@gmail.com>
2016-02-07 23:37:18 -0800

    bug fix

--- background.js
+++ background.js
@@ -5,14 +5,14 @@ chrome.commands.onCommand.addListener(function(command) {
                 if ((typeof items.authToken != "undefined") && (typeof items.authToken != "undefined")) {
                     var xhr = new XMLHttpRequest();
                     var requestUri = "https://api.pinboard.in/v1/posts/add";
-                    requestUri += "?auth_token=" + encodeURI(items.authToken);
-                    requestUri += "&url=" + encodeURI(tab.url);
-                    requestUri += "&description=" + encodeURI(tab.title);
-                    xhr.open("GET", requestUri , true);
+                    requestUri += "?auth_token=" + encodeURIComponent(items.authToken);
+                    requestUri += "&url=" + encodeURIComponent(tab.url);
+                    requestUri += "&description=" + encodeURIComponent(tab.title);
+                    xhr.open("GET", requestUri, true);
                     xhr.send();
                 }
                 else {
-                    alert("Please enter your Pinboard credentials into the extension options");
+                    alert("Please enter your Pinboard credentials into the extension options.");
                 }
             });
         });
