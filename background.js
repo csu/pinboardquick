f2a93b92e517825eacdb0a200de61b4963c61489
Christopher Su <christophersu9@gmail.com>
2016-02-25 12:25:40 -0800

    indentation

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
 chrome.commands.onCommand.addListener(function(command) {
-    if (command == "bookmark-tab") {
-        chrome.tabs.getSelected(null, function(tab){
-            chrome.storage.sync.get(["authToken"], function(items) {
-                if ((typeof items.authToken != "undefined") && (typeof items.authToken != "undefined")) {
-                    var xhr = new XMLHttpRequest();
-                    var requestUri = "https://api.pinboard.in/v1/posts/add";
-                    requestUri += "?auth_token=" + encodeURIComponent(items.authToken);
-                    requestUri += "&url=" + encodeURIComponent(tab.url);
-                    requestUri += "&description=" + encodeURIComponent(tab.title);
-                    xhr.open("GET", requestUri, true);
-                    xhr.send();
-                }
-                else {
-                    alert("Please enter your Pinboard credentials into the extension options.");
-                }
-            });
-        });
-    }
+  if (command == "bookmark-tab") {
+    chrome.tabs.getSelected(null, function(tab){
+      chrome.storage.sync.get(["authToken"], function(items) {
+        if ((typeof items.authToken != "undefined") && (typeof items.authToken != "undefined")) {
+          var xhr = new XMLHttpRequest();
+          var requestUri = "https://api.pinboard.in/v1/posts/add";
+          requestUri += "?auth_token=" + encodeURIComponent(items.authToken);
+          requestUri += "&url=" + encodeURIComponent(tab.url);
+          requestUri += "&description=" + encodeURIComponent(tab.title);
+          xhr.open("GET", requestUri, true);
+          xhr.send();
+        }
+        else {
+          alert("Please enter your Pinboard credentials into the extension options.");
+        }
+      });
+    });
+  }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 var filesToInclude = [
-    './*.png',
-    './*.js',
-    './*.html',
-    './manifest.json'
+  './*.png',
+  './*.js',
+  './*.html',
+  './manifest.json'
 ];
 
 gulp.task('default', ['build']);
 
 gulp.task('build', function () {
-    return gulp.src(filesToInclude, { base: './' })
-        .pipe(zip('release.zip'))
-        .pipe(gulp.dest('.'));
+  return gulp.src(filesToInclude, { base: './' })
+    .pipe(zip('release.zip'))
+    .pipe(gulp.dest('.'));
 });
